8a4341360fe96d43ebd51b47bc72eac0ddc4aeaf
Ayush Daga <dagaayush1205@gmail.com>
2024-12-14 23:55:36 +0530

    included parameter for new armDiff,(.2.2.2.2.2,.5.5.5)

--- receive2.py
+++ receive2.py
@@ -9,7 +9,6 @@ ser = serial.Serial('/dev/serial/by-id/usb-ZEPHYR_Team_RUDRA_Tarzan_33395111002A
 lib = ctypes.CDLL("codegen/dll/armvone/armvone.so")
 class Data(ctypes.Structure):
     _fields_=[
-        ("baseLink",ctypes.c_double),
         ("turntableLink", ctypes.c_double),
         ("linkOne" , ctypes.c_double),
         ("linkTwo" , ctypes.c_double),
@@ -22,14 +21,14 @@ class Data(ctypes.Structure):
 
 DataStruct = Data()
 def initialize():
-    DataStruct.baseLink = 8.0
-    DataStruct.turntableLink = 0.0
-    DataStruct.linkOne = 4.0
-    DataStruct.linkTwo = 5.0
-    DataStruct.endeffector = 2.0
-    DataStruct.x = 1.0
-    DataStruct.y = 1.0
-    DataStruct.z = 1.0
+    DataStruct.turntableLink = 0.2
+    DataStruct.linkOne = 0.2
+    DataStruct.linkTwo = 0.2
+    DataStruct.pitch = 0.2
+    DataStruct.roll = 0.2
+    DataStruct.x = 0.5
+    DataStruct.y = 0.5
+    DataStruct.z = 0.5
 
 def read():
     rcd = ser.read(74)
@@ -37,17 +36,17 @@ def read():
     decoded = cobs.decode(rcd)
 
     DataStruct = ctypes.cast(decoded , ctypes.POINTER(Data))
-    print(DataStruct.contents.baseLink , DataStruct.contents.turntableLink , DataStruct.contents.linkOne , DataStruct.contents.linkTwo , DataStruct.contents.pitch , DataStruct.contents.roll , DataStruct.contents.x , DataStruct.contents.y , DataStruct.contents.z)
+    print(DataStruct.contents.turntableLink , DataStruct.contents.linkOne , DataStruct.contents.linkTwo , DataStruct.contents.pitch , DataStruct.contents.roll , DataStruct.contents.x , DataStruct.contents.y , DataStruct.contents.z)
     return DataStruct
 def Calculate():
-    dv = ctypes.c_double * 6
-    r_dv = dv(DataStruct.baseLink , DataStruct.turntableLink , DataStruct.linkOne , DataStruct.linkTwo , DataStruct.pitch , DataStruct.roll)
+    dv = ctypes.c_double * 5
+    r_dv = dv( DataStruct.turntableLink , DataStruct.linkOne , DataStruct.linkTwo , DataStruct.pitch , DataStruct.roll)
     dv1 = ctypes.c_double * 3
     r_dv1 = dv1(DataStruct.x , DataStruct.y , DataStruct.z)
     vone_size = ctypes.c_double * 2
     r_vone_size = vone_size(0.0 , 0.0)
-    vone_data = ctypes.c_double * 6
-    r_vone_data = vone_data(0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0)
+    vone_data = ctypes.c_double * 5
+    r_vone_data = vone_data( 0.0 , 0.0 , 0.0 , 0.0 , 0.0)
     lib.armvone.argtypes = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
     result = lib.armvone(r_dv , r_dv1 , r_vone_data , r_vone_size)
     print("OUTPUT: ",*r_vone_data)
@@ -63,11 +62,11 @@ def send():
 def main():
     initialize()
     # while True:
-    read()
+    # read()
     Calculate()
-    while True:
-        send()
-        time.sleep(2.0)
+    # while True:
+    #     send()
+    #     time.sleep(2.0)
         # test = Calculate() 
         # send(test)
 if __name__ =='__main__':
