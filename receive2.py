37ea1f591f5cfd4fde2cec6a8647df02cedaaa56
Ayush Daga <dagaayush1205@gmail.com>
2024-12-15 15:50:44 +0530

    1. struct changed 2. passing coordinate value 3. joystick init 4. joystick control 5. THIS IS NOT TESTED

--- receive2.py
+++ receive2.py
@@ -3,13 +3,35 @@ import serial
 import time
 import struct
 from cobs import cobs
+import pygame
 
+pygame.init()
+pygame.joystick.init()
 
-ser = serial.Serial(
-    '/dev/serial/by-id/usb-ZEPHYR_Team_RUDRA_Tarzan_33395111002A0038-if00', 9600, timeout=1, write_timeout=1)
-# lib = ctypes.CDLL("codegen/dll/armvone/armvone.so")
+def joystickinit():
+    if pygame.joystick.get_count() == 0:
+        print("No joystick detected!")
+        pygame.quit()
+        exit()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    print(f"Joystick '{joystick.get_name()}' initialized")
+    return joystick
 
+def joystickread(joystick):
+    x = 0
+    y = 0
+    z = 0
+    for event in pygame.event.get():
+        x = joystick.get_axis(0)
+        y = joystick.get_axis(1)
+        z = joystick.get_axis(2)
+        print(f"x:{x:.2f} y:{y:.2f} z:{z:.2f} ")
+    return x , y , z
 
+ser = serial.Serial('/dev/serial/by-id/usb-ZEPHYR_Team_RUDRA_Tarzan_33395111002A0038-if00', 9600)
+# ser = serial.Serial('/dev/pts/6')
+lib = ctypes.CDLL("codegen/dll/armvone/armvone.so")
 class Data(ctypes.Structure):
     _fields_ = [
         ("baseLink", ctypes.c_double),
@@ -23,20 +45,24 @@ class Data(ctypes.Structure):
         ("z", ctypes.c_double),
     ]
 
-datastruct = Data()
-def initialize():
-    datastruct.turntableLink = 0.2
-    datastruct.linkOne = 0.2
-    datastruct.linkTwo = 0.2
-    datastruct.pitch = 0.2
-    datastruct.roll = 0.2
-    datastruct.x = 0.5
-    datastruct.y = 0.5
-    datastruct.z = 0.5
+# datastruct = Data()
+# def initialize():
+#     datastruct.turntableLink = 0.2
+#     datastruct.linkOne = 0.2
+#     datastruct.linkTwo = 0.2
+#     datastruct.pitch = 0.2
+#     datastruct.roll = 0.2
+#     datastruct.x = 0.5
+#     datastruct.y = 0.5
+#     datastruct.z = 0.5
 
 def home():
-    current = Data()
     current = read()
+    current.contents.x = 0.5
+    current.contents.y = 0.5
+    current.contents.z = 0.5 
+    output = Calculate(current.contents.x , current.contents.y ,current.contents.z)
+    send(output)
 
 def read():
     if ser.in_waiting > 0:
@@ -51,23 +77,26 @@ def read():
         return
 
 
-def Calculate():
+def Calculate(x , y , z):
+    ikstruct = Data()
+    ikstruct = read()
+    ikstruct.contents.x = x
+    ikstruct.contents.y = y
+    ikstruct.contents.z = z
     dv = ctypes.c_double * 5
-    r_dv = dv( datastruct.turntableLink , datastruct.linkOne , datastruct.linkTwo , datastruct.pitch , datastruct.roll)
+    r_dv = dv( ikstruct.contents.turntableLink , ikstruct.contents.linkOne , ikstruct.contents.linkTwo , ikstruct.contents.pitch , ikstruct.contents.roll)
     dv1 = ctypes.c_double * 3
-    r_dv1 = dv1(datastruct.x , datastruct.y , datastruct.z)
+    r_dv1 = dv1(ikstruct.contents.x , ikstruct.contents.y , ikstruct.contents.z)
     vone_size = ctypes.c_double * 2
     r_vone_size = vone_size(0.0, 0.0)
     vone_data = ctypes.c_double * 5
-    r_vone_data = vone_data(0.0, 0.0, 0.0, 0.0, 0.0)
-    lib.armvone.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(
-        ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
-    result = lib.armvone(r_dv, r_dv1, r_vone_data, r_vone_size)
-    print("OUTPUT: ", *r_vone_data)
+    r_vone_data = vone_data(0.0 , 0.0 , 0.0 , 0.0 , 0.0)
+    lib.armvone.argtypes = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
+    result = lib.armvone(r_dv , r_dv1 , r_vone_data , r_vone_size)
+    print("OUTPUT: ",*r_vone_data)
     return result
 
-
-def send():
+def send(datastruct):
     encoded_data = cobs.encode(bytearray(datastruct))
     print(encoded_data)
     ser.write(encoded_data)
@@ -76,13 +105,37 @@ def send():
 
 
 def main():
-    initialize()
+    controller=joystickinit()
     home()
-    # while True:
-    # read()
-    Calculate()
-    send()
-    time.sleep(0.001)
+    x = 0.5
+    y = 0.5
+    z = 0.5
+    running = True
+    newx = 0.0
+    newy = 0.0
+    newz = 0.0
+    while running:
+        dirx , diry , dirz = joystickread(controller)
+        if dirx > 0.3:
+            newx = x+0.05
+        elif dirx < -0.3:
+            newx = x-0.05
+        
+        if diry > 0.3:
+            newy = y+0.05
+        elif diry < -0.3:
+            newy = y-0.05
 
-if __name__ == '__main__':
+        if dirz > 0.3:
+            newz = z+0.05
+        elif dirz < -0.3:
+            newz = z-0.05
+
+        output = Calculate(newx , newy , newz)
+        send(output)
+        x = newx
+        y = newy
+        z = newz
+
+if __name__ =='__main__':
     main()
